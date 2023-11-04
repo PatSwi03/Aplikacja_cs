763d3065ddd8758c741b639b1010158d39825dff
Patryk Świetlik <patrykswietlik2003@gmail.com>
2023-11-04 11:08:53 +0100

    Po_lekcjach

--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
-﻿using System.IO;
+﻿using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq.Expressions;
+
 internal class Program
 {
 
+
+    static int a = 0, b = 0;
+    static string pobrana;
 
     /// <summary>
     /// Procedura główna aplikacji
@@ -10,22 +16,30 @@ internal class Program
 
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-        Kielbasa();
-        Console.ReadLine();
-        
-    }
+        try
+        {
+            Console.WriteLine("Podaj liczbę a: ");
+            a = int.Parse(Console.ReadLine()!);
 
+            Console.WriteLine("Podaj liczbę b: ");
+            b = int.Parse(Console.ReadLine()!);
 
-    /// <summary>
-    /// 
-    /// </summary>
-    private static void Kielbasa()
-    {
-        Console.WriteLine($"Kielbasa");
+            Console.WriteLine(a + b);
+
+            Console.ReadLine();
+
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Stary podałeś coś nie tak. ");
+        }
+        }
+   // private static void Kielbasa()
+    //{
+     //   Console.WriteLine($"Kielbasa");
         //TextReader tr = File.OpenText("D:\\Kodzik2\\ConsoleApp1\\README.md");
         //Console.WriteLine(tr.ReadToEnd());
-    }
+   // }
 
 
 }
