c85b6955fc9a74894c6b00d6057fb3c02611a443
Patryk Świetlik <patrykswietlik2003@gmail.com>
2023-11-04 09:38:56 +0100

    Stworzenie metody kielbasa

--- Program.cs
+++ Program.cs
@@ -1,7 +1,31 @@
-﻿internal class Program
+﻿using System.IO;
+internal class Program
 {
+
+
+    /// <summary>
+    /// Procedura główna aplikacji
+    /// </summary>
+    /// <param name="args">Parametry wejsciowe w postacji tablicy tekstowej</param>
+
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        Kielbasa();
+        Console.ReadLine();
+        
     }
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    private static void Kielbasa()
+    {
+        Console.WriteLine($"Kielbasa");
+        //TextReader tr = File.OpenText("D:\\Kodzik2\\ConsoleApp1\\README.md");
+        //Console.WriteLine(tr.ReadToEnd());
+    }
+
+
 }
